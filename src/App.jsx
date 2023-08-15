26d744bf67906e1fe3746e2d6fed83250e7cb24f
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-08-15 14:42:57 -0700

    fetch users

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
- 
+import { useEffect, useState } from "react";
+import "./App.css";
+import Users from "./components/Users";
+
+function App() {
+  const API = "https://reqres.in/api/users";
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div className="App">
+      <Users items={users} isLoading={isLoading} />
+      {/* <Success /> */}
+    </div>
+  );
+}
+
+export default App;
